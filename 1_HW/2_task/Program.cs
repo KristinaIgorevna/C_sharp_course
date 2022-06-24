eabd05375a85e2abf64629fdc7a0f30be9bfab08
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-25 00:53:13 +0300

    максимум трех чисел

---
+++ 1_HW/2_task/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a>b && a>c)
+{ 
+    
+Console.Write("max=");
+Console.Write(a);
+    
+}
+else if (b>c)
+{
+Console.Write("max=");
+Console.Write(b);
+}
+else 
+{
+Console.Write("max=");
+Console.Write(c);
+}
+
+
+
+
