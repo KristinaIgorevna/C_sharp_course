7ac68f42167b9bf4498d724c90415366d73010aa
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-27 22:02:29 +0300

    2 задание решено

--- 2_HW/2_2HW/Program.cs
+++ 2_HW/2_2HW/Program.cs
@@ -1,11 +1,12 @@
 ﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
 
 
-int SecondNum = new Random().Next(100, 999);
-
-
-Console.WriteLine(SecondNum);
-Console.WriteLine(SecondNum % 10 > SecondNum / 10);
+int Num_3 = new Random().Next(100, 999);
+int Num_1 = (Num_3  / 100);
+int Num_2 = (Num_3 % 10);
+Console.WriteLine(Num_3);
+Console.Write(Num_1);
+Console.Write(Num_2);
 
 
 
