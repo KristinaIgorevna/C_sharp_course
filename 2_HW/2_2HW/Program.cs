82e26f773a92dbedfce95ce39789a303f96bb815
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-27 21:41:22 +0300

    что-то получилось

---
+++ 2_HW/2_2HW/Program.cs
@@ -0,0 +1,11 @@
+﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+
+int SecondNum = new Random().Next(100, 999);
+
+
+Console.WriteLine(SecondNum);
+Console.WriteLine(SecondNum % 10 > SecondNum / 10);
+
+
+
---
+++ 2_HW/2_3HW/Program.cs
@@ -0,0 +1,20 @@
+﻿void OneOfNum(int num_1)
+{
+    if (num_1 > 99 && num_1 < 1000)
+    {
+        Console.WriteLine(num_1 % 10);
+    }
+
+    else if (num_1 > 999)
+    {
+        Console.WriteLine(num_1 / 10);
+    }
+
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
+OneOfNum(16);
+OneOfNum(161);
+OneOfNum(1617345);
