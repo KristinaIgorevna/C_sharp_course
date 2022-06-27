25d38612c038739a30167036d6298a27088aac21
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-27 22:24:07 +0300

    4 задание выполнено

--- 2_HW/2_4HW/Program.cs
+++ 2_HW/2_4HW/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает цифру, обозначающую день недели и проверяет- выходной
+
+
+int Day = int.Parse(Console.ReadLine());
+
+if (Day ==6)
+{
+    Console.WriteLine($"{Day}->да");
+}
+else if (Day ==7)
+{
+    Console.WriteLine($"{Day}->да");
+}
+else
+{
+    Console.WriteLine($"{Day}->нет");
+}
