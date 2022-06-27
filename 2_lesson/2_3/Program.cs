45e7429969b5205c1050c77bae5898d1303e5414
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-27 11:15:49 +0300

    3 задача

--- 2_lesson/2_3/Program.cs
+++ 2_lesson/2_3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static int CheckinValues(in int val1, in int val2)
+        {
+            return (val1 % val2);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число ");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int value2 = int.Parse(Console.ReadLine());
+
+            int val = CheckinValues(in value1, in value2);
+
+            if (val == 0)
+            {
+                Console.WriteLine("Числа кратные");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток от деления {val}");
+            }
+        }
