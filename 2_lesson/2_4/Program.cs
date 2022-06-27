0d82956fa9478cf566fdf2bb0bf2c0447be589d0
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-27 11:24:06 +0300

    4 решение

---
+++ 2_lesson/2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿void OneOfNum(int num_1)
+{       
+    if (num_1 % 7 == 0 && num_1 % 23 == 0 )
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");        
+    }    
+}
+
+OneOfNum(16);
+OneOfNum(161);
+
+
+// 4. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
