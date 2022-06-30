a0d9c2dc6af84f1bbabc896d8e0b257ba214c21f
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-06-30 23:46:44 +0300

    1task не верно жду комментарии

--- 3_HW/3_1hw/Program.cs
+++ 3_HW/3_1hw/Program.cs
@@ -1,2 +1,20 @@
-﻿// 
+﻿// принимает на вход пятизначное число и 
+// проверяет является ли оно палиндромом
 
+
+void Num(int num)
+{
+
+int index = 0;
+while (index < 5)
+{
+  if (index[1]= index[5] & index[2] = index[4])
+{
+Console.WriteLine("да");
+}
+else Console.WriteLine("нет");  
+index++;
+}
+
+}
+Num(12421);
