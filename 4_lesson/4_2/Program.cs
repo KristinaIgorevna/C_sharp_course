660d792394efc734069caa10b7cf280d966fa067
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-07-04 10:24:27 +0300

    2 решение

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// // 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+
+
+int CountNum(int num)
+{
+    string result = num.ToString();
+    return result.Length;
+}
+Console.WriteLine(CountNum(456));
