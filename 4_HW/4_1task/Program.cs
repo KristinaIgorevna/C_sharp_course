8fff0667f94d4e348279650e64d50751052ac3f5
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-07-07 10:05:34 +0300

    еще не решила правильно

---
+++ 4_HW/4_1task/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите цикл, который принимает на вход два числа А ,В и возводит А в натуральную степень В
+
+
+int B (int num)
+{
+    int A = 3;
+    {
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine(Math.Pow(A, i));
+
+    }
+    return A;
+    }
+}
+
+Console.WriteLine(A(5));
