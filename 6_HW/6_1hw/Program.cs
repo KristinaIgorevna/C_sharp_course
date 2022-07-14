7b4458f9fdad6b8042689e1cedd47894a15cb365
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-07-15 01:21:01 +0400

    решения

---
+++ 6_HW/6_1hw/Program.cs
@@ -0,0 +1,19 @@
+﻿// пользователь вводит с клавиатуры М чисел
+//посчитать сколько чисел > 0 ввел
+
+
+
+int [] array = Array.ConvertAll (Console.ReadLine().Split(), int.Parse);
+{
+
+int count=0;
+for (int i=0; i<array.Length; i ++)
+{
+    if (array[i] >0) 
+    {
+       count ++; 
+    }
+}
+
+   Console.WriteLine (count); 
+}
---
+++ 6_HW/6_2hw/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу которая найдет точку пересечения двух прямых
+// заданных уравнениями у=к1*x+b1 y=k2*x+b2
+
+
+void dot(double b1, double b2, double k1, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка перенсечения: ({x};{y})");
+
+
+}
+dot(3, 5, 2, 4);
