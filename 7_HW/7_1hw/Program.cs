938dc7bf0667fcb0650d808d2575685251ee2896
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-07-16 23:22:46 +0400

    1 задание

---
+++ 7_HW/7_1hw/Program.cs
@@ -0,0 +1,35 @@
+﻿// задайте двумерный массив размером m*n
+// заполненный случайными вещественными числами
+
+
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetHashCode(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; i < column_size; j++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+    double[,] MassNums(int row, int column, int from, int to)
+    {
+        double[,] arr = new double[row, column];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                arr[i, j] = new Random().Next(from, to);
+            }
+        }
+        return arr;
+    }
+
+int[,] arr_1 = MassNums(3, 4, -99, 100);
+Print (arr_1);
+     
