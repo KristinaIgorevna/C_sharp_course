05884198796192535fb43b7c8c766a60eed7fc81
Meline Karapetyan <melinekarapetyan@MacBook-Pro-Meline.local>
2022-07-21 11:52:29 +0400

    2  решение

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая рекурсивно выведет все натуральные числа в промежутке от M до N.
+// M = 1; 
+ // N = 5 -> "1, 2, 3, 4, 5" M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Sequence(int M, int N)
+{
+    if (N < M) return;
+    Sequence(M, N-1);
+    Console.Write($"{N}, ");
+}
+Sequence(M, N);
